1a3e8889fb04f1e50aa8d30a075ac0cadee5d7d8
Jsoto2 <joshuasoto276@gmail.com>
2021-05-19 20:00:16 -0400

    added for my gallery

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,3 +1,14 @@
 // You can include your own javascript here
 // alert("hello, your main.js file was added properly.");
 console.log("Logging from main.js, it was loaded.");
+
+
+function switchStyle() {
+    if (document.getElementById('styleSwitch').checked) {
+      document.getElementById('gallery').classList.add("custom");
+      document.getElementById('exampleModal').classList.add("custom");
+    } else {
+      document.getElementById('gallery').classList.remove("custom");
+      document.getElementById('exampleModal').classList.remove("custom");
+    }
+  }
